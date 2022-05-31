cd18fb2a23b0134190bb51b7ddfaa2676e5b9678
Jaroslaw Charchula <charchula@gmail.com>
2022-05-31 23:49:10 +0200

    set proper project files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react'
 
-const Person = (props) => {
+function App() {
   return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  );
-}
-
-const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("You've changed the counter to" + counter);
-  }, [counter]);
-
-
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount-1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() =>  setCounter((prevCount) => prevCount+1)}>+</button>
+    <div className='container'> 
+        // info
+        // About
+        // Interest
+        // Footer
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
