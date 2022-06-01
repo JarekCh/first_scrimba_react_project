cc624e6796b2e105157ffc866e725bdc50498ad4
Jaroslaw Charchula <charchula@gmail.com>
2022-06-01 23:02:06 +0200

    made info component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import About from './components/About'
 import Interest from './components/Interest'
 import Footer from './components/Footer'
 
+import './App.css'
+
 function App() {
   return (
     <div className='container'> 
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import anastasiaPhoto from "../img/anastasia-shuraeva.jpg"
+import linkedin from "../img/linkedin.png"
+import mail from "../img/Mail.png"
 
 function Info() {
   return (
-    <div className='info__conatainer'>
-      <div className='abc' > sadsadas</div>
-      <img src='../src/img/anastasia-shuraeva.jpg' alt=""/>
+    <div className='info__container'>      
+      <img src={anastasiaPhoto} alt=""
+      className='info__picture'/>
+      <h2 className='info__fullName'>Anastasia Shura</h2>
+      <p className='info__jobPossition'>Frontend Developer</p>
+      <p className='info__website'>anashura.website</p>
+      <div className='info__btnContainer'>
+        <button className='info__mailBtn'>
+          <img src={mail} />
+          <span>Email</span>
+        </button>
+        <button className='info__linkedinBtn'>
+          <img src={linkedin} />
+          <span>LinkedIn</span>
+        </button>        
+      </div>
+
     </div>
+
   )
 }
 
